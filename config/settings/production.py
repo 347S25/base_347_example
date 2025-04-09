c73884e69a52b6f202254ac3e1116251258611f6
Michael Stewart <stewarmc@jmu.edu>
2025-04-08 23:17:38 -0400

    try uvicorn

--- config/settings/production.py
+++ config/settings/production.py
@@ -48,7 +48,7 @@ CACHES = {
 # https://docs.djangoproject.com/en/dev/ref/settings/#secure-proxy-ssl-header
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 # https://docs.djangoproject.com/en/dev/ref/settings/#secure-ssl-redirect
-SECURE_SSL_REDIRECT = env.bool("DJANGO_SECURE_SSL_REDIRECT", default=True)
+SECURE_SSL_REDIRECT = env.bool("DJANGO_SECURE_SSL_REDIRECT", default=False)
 # https://docs.djangoproject.com/en/dev/ref/settings/#session-cookie-secure
 SESSION_COOKIE_SECURE = True
 # https://docs.djangoproject.com/en/dev/ref/settings/#session-cookie-name
@@ -188,7 +188,7 @@ LOGGING = {
 
 # Your stuff...
 # ------------------------------------------------------------------------------
-DEBUG = False
+DEBUG = True
 
 
 if sys.argv[1] != 'collectstatic':
